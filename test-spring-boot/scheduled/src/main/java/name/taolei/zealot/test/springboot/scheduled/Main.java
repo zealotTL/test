b64b7fdd8741de2bd8fb2a78775df310ce56af81
zealot <zealotTL@users.noreply.github.com>
2017-12-14 13:31:35 +0800

    默认修改格式

--- test-spring-boot/scheduled/src/main/java/name/taolei/zealot/test/springboot/scheduled/Main.java
+++ test-spring-boot/scheduled/src/main/java/name/taolei/zealot/test/springboot/scheduled/Main.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling @Configuration @ComponentScan("name.taolei.zealot.test.springboot.scheduled") public class Main {
+@EnableScheduling
+@Configuration
+@ComponentScan("name.taolei.zealot.test.springboot.scheduled")
+public class Main {
 
     public static void main(String[] ags) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
--- test-spring-boot/scheduled/src/main/java/name/taolei/zealot/test/springboot/scheduled/TestScheduledService.java
+++ test-spring-boot/scheduled/src/main/java/name/taolei/zealot/test/springboot/scheduled/TestScheduledService.java
@@ -6,14 +6,18 @@ import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-@Service public class TestScheduledService {
-    private final SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
+@Service
+public class TestScheduledService {
 
-    @Scheduled(fixedRate = 5 * 1000) public void one() {
+    @Scheduled(fixedRate = 5 * 1000)
+    public void one() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
         System.out.println("fixedRate = 5 *1000  " + sdf.format(Calendar.getInstance().getTime()));
     }
 
-    @Scheduled(cron = "0/5 * * * * *") public void two() {
+    @Scheduled(cron = "0/5 * * * * *")
+    public void two() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
         System.out.println("cron = \"0/5 * * * * *\"  " + sdf.format(Calendar.getInstance().getTime()));
     }
 }
