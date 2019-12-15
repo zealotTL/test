0830020d0e343c1df5a866cf6466894a970d936f
zealotTL <t953170224@163.com>
2019-12-15 21:02:33 +0800

    增加mongodb

---
+++ datasource/src/test/java/group/zealot/test/TestMain.java
@@ -0,0 +1,61 @@
+package group.zealot.test;
+
+import com.alibaba.fastjson.JSONObject;
+import com.mongodb.client.result.UpdateResult;
+import group.zealot.test.datasource.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Main.class})
+@Component
+public class TestMain {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired(required = false)
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void send() throws InterruptedException {
+        if (!mongoTemplate.collectionExists("user")) {
+            logger.info("创建 user 集合");
+            mongoTemplate.createCollection("user");
+        }
+
+        List<JSONObject> list = mongoTemplate.findAll(JSONObject.class, "user");
+
+        Query query = Query.query(Criteria.where("name").is("tao"));
+
+        JSONObject tao = mongoTemplate.findOne(query, JSONObject.class, "user");
+
+        Update update = Update.update("age", 15);
+        UpdateResult updateResult = mongoTemplate.updateFirst(query, update, "user");
+/*
+        tao = new JSONObject();
+        tao.put("name", "tao");
+        tao.put("age", 17);
+        tao.put("sex", "男");
+        mongoTemplate.insert(tao, "user");
+*/
+
+        logger.info("over");
+    }
+
+
+}
