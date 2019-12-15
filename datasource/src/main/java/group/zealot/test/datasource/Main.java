59d7606aa327b05d7f2744f53ac269ba24e5e1d0
zealotTL <t953170224@163.com>
2019-12-15 21:42:02 +0800

    mongodb 连接池配置

--- datasource/src/main/java/group/zealot/test/datasource/Main.java
+++ datasource/src/main/java/group/zealot/test/datasource/Main.java
@@ -1,11 +1,13 @@
 package group.zealot.test.datasource;
 
+import com.spring4all.mongodb.EnableMongoPlus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
+@EnableMongoPlus
 public class Main {
 
     public static void main(String[] args) {
